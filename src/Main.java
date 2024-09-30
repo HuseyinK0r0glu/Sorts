8f32aead8cc316fe6c8607fc22eb34e439e34241
HuseyinK0r0glu <korogluh27@gmail.com>
2024-09-30 19:54:48 +0300

    merge sort added

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         int[] array = createArray();
-        bubbleSort(array);
+
+        int[] helper = new int[array.length];
+
+        mergeSort(array,helper,0,array.length-1);
+        //bubbleSort(array);
         //insertionSort(array);
         //selectionSort(array);
         writeArray(array);
@@ -60,6 +64,42 @@ public class Main {
             if(!swapped){
                 break;
             }
+        }
+    }
+
+    public static void mergeSort(int[] array,int[] helper,int low,int high){
+        if(low < high){
+            int middle = (low + high)/2;
+            mergeSort(array,helper,low,middle); // for the left part
+            mergeSort(array,helper,middle + 1,high); // for the right part
+            merge(array,helper,low,middle,high);
+        }
+    }
+
+    public static void merge(int[] array,int[] helper,int low,int middle,int high){
+        for(int i = 0;i<array.length;i++){
+            helper[i] = array[i];
+        }
+
+        int helperLeft = low;
+        int helperRight = middle+1;
+        int current = low;
+
+        while(helperLeft <= middle && helperRight <=high){
+            if(helper[helperLeft] <= helper[helperRight]){
+                array[current] =  helper[helperLeft];
+                helperLeft++;
+            }else {
+                array[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        int remaining = middle - helperLeft;
+        // there is equal sign before the remaining because we should add the middle one too
+        for(int i = 0;i<=remaining;i++){
+            array[current + i] = helper[helperLeft + i];
         }
     }
 
